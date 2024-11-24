d8b60303d6265f57fe10f358fcd6f6d82c93041b
Adam Chojaczyk <adam.chojaczyk@epicvr.pl>
2024-11-24 08:11:36 +0100

    Trying to fix wrong build directory in production

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            publicDirectory: './public_html',
-            hotFile: './public_html/hot',
+            publicDirectory: 'public_html/',
+            hotFile: 'public_html/hot',
             buildDirectory: 'build',
             refresh: true,
         }),
         vue(),
     ],
-    /*build: {
-        outDir: 'public_html/build',
-    },*/
+    build: {
+        // Absolutna ścieżka do katalogu public_html/build
+        outDir: path.resolve(__dirname, 'public_html/build'),
+        emptyOutDir: true, // Czyści katalog przed buildem
+    },
 });
